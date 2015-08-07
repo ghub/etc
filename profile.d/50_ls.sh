6f372cea9d2a056a2481919962628fcffcc70bed
Ge Wang <code.gewang@gmail.com>
2015-08-06 23:35:38 -0600

    Minor clean up

--- profile.d/50_ls.sh
+++ profile.d/50_ls.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 
-if [ `uname -s` = "Darwin" ]; then
+if [ $( uname -s ) = Darwin ]; then
     alias ls='ls -G'
 fi
--- profile.d/50_vi.sh
+++ profile.d/50_vi.sh
@@ -3,6 +3,6 @@
 VISUAL=vi
 export VISUAL
 
-if [ `uname -s` = "Darwin" ]; then
+if [ $( uname -s ) = Darwin ]; then
     alias vi='mvim -v'
 fi
