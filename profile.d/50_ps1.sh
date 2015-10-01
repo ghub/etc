bd1f1a0e9c32fa5585ab0f98f745039dce8c02e2
Ge Wang <code.gewang@gmail.com>
2015-09-30 21:12:06 -0600

    Tweak PS1

--- profile.d/50_ps1.sh
+++ profile.d/50_ps1.sh
@@ -5,8 +5,8 @@ get_status()
     status=$?
 
     if [ $status -ne 0 ]; then
-        echo "$(tput setaf 1)$status$(tput sgr0) "
+        echo "$(tput setaf 1)$status$(tput sgr0)"
     fi
 }
 
-export PS1="\$(get_status)$PS1"
+export PS1="\$(get_status)\n$PS1"
