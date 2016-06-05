8316571d55cbb081ed94facfba63c4a86a63b1b7
Ge Wang <code.gewang@gmail.com>
2016-06-05 16:47:16 -0600

    Fix bad quote

--- profile.d/50_docker.sh
+++ profile.d/50_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 if [ $( uname -s ) = Darwin ]; then
-    for each in "/Applications/Docker.app/Contents/Resources/etc/*.bash-completion"; do
+    for each in /Applications/Docker.app/Contents/Resources/etc/*.bash-completion; do
         . $each
     done
 fi
