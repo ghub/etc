6e615ec92f06f77740e7bcd418c6ce093ad0ae76
Ge Wang <code.gewang@gmail.com>
2019-05-01 20:39:43 -0600

    Only load docker bash completion if available

--- profile.d/50_docker.sh
+++ profile.d/50_docker.sh
@@ -2,6 +2,7 @@
 
 if [ $( uname -s ) = Darwin ]; then
     for each in /Applications/Docker.app/Contents/Resources/etc/*.bash-completion; do
+        [ -f "$each" ] || continue
         . $each
     done
 fi
